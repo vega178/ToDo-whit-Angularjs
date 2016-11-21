b7c66b35becec3f9af67e3cbef71c6ce83b16156
yeoman-generator-angularjs <estebanvegapatio@gmail.com>
2016-11-21 18:34:29 -0500

    Agregar tareas con grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,19 @@
-module.exports = function (grunt){
+module.exports = function (grunt) {
     //configuración del inicio del proyecto
     grunt.initConfig({
         //Configuración basica e información del plugin
         pkg: grunt.file.readJSON('package.json'),
+
+        // crear un servidor
+        connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    port: 9010,// necesarion para dejar el servidor en espera 
+                    keepalive: true,
+                },
+            }
+        },
 
         sass: {
             options: {
@@ -16,14 +27,26 @@ module.exports = function (grunt){
             }
         },
         watch: {
+            //options:{livereload:true},
             sass: {
                 files: 'css/**.sass',
                 tasks: 'sass'
+            },
+            css: {
+                files: ['css/**/*.css'],
+                tasks: ['watch'],
+                /*options: {
+                    spawn: false,
+                    livereload: true
+                }*/
             }
         }
     });
+    //Cargar el plugin
      grunt.loadNpmTasks('grunt-contrib-sass');
      grunt.loadNpmTasks('grunt-contrib-watch');
-
+     grunt.loadNpmTasks('grunt-contrib-connect');
+     ////Hacer o ejecutar la tarea
      grunt.registerTask('default', ['watch']);
+     grunt.registerTask('default', ['connect']);
 };
